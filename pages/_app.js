f65ce356aad4a3b7a270277155bc7f0a05d9d781
Pooja Soliwal <poojasoliwal2222@gmail.com>
2021-09-06 09:44:54 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import Theme from "/theme.js";
+import "../styles/small.css";
 export default function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={Theme}>
